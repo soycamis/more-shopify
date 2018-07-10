de76989080ca9d958e8500f7edb082ccc4b1630f
Camilo Sosa <camiilososa@gmail.com>
2018-07-10 12:46:12 -0500

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,35 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const prefix = require("gulp-autoprefixer");
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const imagemin = require('gulp-imagemin')
+const pngquant = require('imagemin-pngquant');
+const prefix = require('gulp-autoprefixer')
 
-const input = "./sass/*.scss";
-const output = "./assets/";
 
-gulp.task("sass", () => {
-  return gulp.src(input)
+const input = './src/*.scss'
+const output = './assets/'
+
+gulp.task('prefix-sass', () => {
+  gulp.src(input)
+    .pipe(prefix({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(sass({
       errLogToConsole: true,
-      outputStyle: "compressed"
-    }).on("error", sass.logError))
-    .pipe(gulp.dest(output));
-});
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest(output))
+})
 
-gulp.task("default", () => {
-  gulp.watch("./sass/*.scss", ["sass"]);
-});
+gulp.task('imagemin', () => {
+  gulp.src('./src/img/**')
+    .pipe(imagemin({
+      progressive: true,
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('./assets/'))
+})
+
+gulp.task('default', () => {
+  gulp.watch('./src/*.scss', ['prefix-sass'])
+})
